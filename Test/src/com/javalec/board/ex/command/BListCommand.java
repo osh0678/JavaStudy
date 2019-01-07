59b890d3f70279ad3327e1ba6ef8b8ea907c9ec0
오성환 <osh0678@naver.com>
2019-01-07 09:55:43 +0900

    2019.01.07 Board Test

---
+++ Test/src/com/javalec/board/ex/command/BListCommand.java
@@ -0,0 +1,22 @@
+package com.javalec.board.ex.command;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.board.ex.dao.BDAO;
+import com.javalec.board.ex.dto.BDTO;
+
+public class BListCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+		BDAO dao = new BDAO();
+		ArrayList<BDTO> dtos = dao.list();
+		request.setAttribute("list", dtos);
+	}
+
+}
---
+++ Test/src/com/javalec/board/ex/command/BWriteCommand.java
@@ -0,0 +1,23 @@
+package com.javalec.board.ex.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.board.ex.dao.BDAO;
+
+public class BWriteCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+		String bName = request.getParameter("bName");
+		String bTitle = request.getParameter("bTitle");
+		String bContent = request.getParameter("bContent");
+		
+		BDAO dao = new BDAO();
+		dao.write(bName, bTitle, bContent);
+
+	}
+
+}
