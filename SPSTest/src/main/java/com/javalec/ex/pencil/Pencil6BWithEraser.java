9b579ee42f91034a98a7b139789acb5283f207a4
osh0678 <34639547+osh0678@users.noreply.github.com>
2019-01-17 10:42:05 +0900

    Add files via upload
    2019.01.17 Spring test

---
+++ SPSTest/src/main/java/com/javalec/ex/pencil/Pencil6BWithEraser.java
@@ -0,0 +1,11 @@
+package com.javalec.ex.pencil;
+
+public class Pencil6BWithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("6B굵기로 쓰이며, 지우개가 있습니다.");
+	}
+
+}
